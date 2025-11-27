5c229440abe5e6bb9598d133859ebb093528c7ae
Sukanya Yakam <sukanya@Sukanyas-MacBook-Air.local>
2025-11-27 11:23:32 +0700

    TS error by removing the unsupported

--- src/diagram/render/edgeTypes/FloatingEdge.tsx
+++ src/diagram/render/edgeTypes/FloatingEdge.tsx
@@ -365,7 +365,7 @@ export function EdgeModel({
         markerStart={appliedMarkerStart}
         markerEnd={appliedMarkerEnd}
         interactionWidth={40}
-        mask={`url(#${maskId})`}
+        pathProps={{ mask: `url(#${maskId})` }}
         className={
           selected ? 'stroke-2 drop-shadow-[0_0_0.25rem_hsl(var(--ring)/0.55)]' : ''
         }
